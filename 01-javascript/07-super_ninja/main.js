f2b04d8663a1025d63c51963c3eff89f31528659
Eduardo Cuellar <eduardoc190@gmail.com>
2023-01-19 02:19:40 -0300

    [TAREA] sensei

--- 01-javascript/07-super_ninja/main.js
+++ 01-javascript/07-super_ninja/main.js
@@ -40,4 +40,4 @@ const superSensei = new Sensei("Master Splinter");
 superSensei.speakWisdom();
 // -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
 superSensei.showStats();
-// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10" 

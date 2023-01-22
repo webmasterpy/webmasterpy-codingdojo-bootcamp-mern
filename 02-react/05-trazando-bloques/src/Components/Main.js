cc7e5398ab16d9b3729de7c9ca778380ef7a1eb1
Eduardo Cuellar <eduardoc190@gmail.com>
2023-01-22 20:39:16 -0300

    Trazando Bloques

---
+++ 02-react/05-trazando-bloques/src/Components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = props => {
+  //const {sub} = this.props;
+
+  return (
+    <div className="main">
+       {props.children}
+    </div>
+  )
+}
+
+export default Main
---
+++ 02-react/05-trazando-bloques/src/Components/SubContents.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SubContents = () => {
+  return (
+    <div className="subContent"></div>
+  )
+}
+
+export default SubContents

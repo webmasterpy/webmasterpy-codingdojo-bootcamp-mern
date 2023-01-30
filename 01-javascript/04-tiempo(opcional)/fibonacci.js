d27344d73442b1a53d0fc6b2dac7654240c897ab
Eduardo Cuellar <eduardoc190@gmail.com>
2023-01-30 08:48:26 -0300

    Fibonacci y reversion de Cadenas

--- 01-javascript/04-tiempo(opcional)/fibonacci.js
+++ 01-javascript/04-tiempo(opcional)/fibonacci.js
@@ -1,20 +1,45 @@
-// recursive
-function rFib( n ) {
-    if ( n < 2 ) {
-        return n;
-    }
-    return rFib( n-1 ) + rFib( n-2 );
-}
-rFib(20);
+// // recursive
+// function rFib( n ) {
+//     if ( n < 2 ) {
+//         return n;
+//     }
+//     return rFib( n-1 ) + rFib( n-2 );
+// }
+// rFib(20);
 
-// iterative
-function iFib( n ) {
-    const vals = [ 0, 1 ];
-    while(vals.length-1 < n) {
-        let len = vals.length;
-        vals.push( vals[len-1] + vals[len-2] );
-    }
-    return vals[n];
-}
-iFib(20);
+// // iterative
+// function iFib( n ) {
+//     const vals = [ 0, 1 ];
+//     while(vals.length-1 < n) {
+//         let len = vals.length;
+//         vals.push( vals[len-1] + vals[len-2] );
+//     }
+//     return vals[n];
+// }
+// iFib(20);
     
+let start = performance.now();
+// recursive
+function rFib(n) {
+    if (n < 2) {
+        return n;
+    }
+    return rFib(n - 1) + rFib(n - 2);
+}
+console.log(rFib(20));
+console.log(`Recursive took ${performance.now() - start} milliseconds to run \n`);
+
+
+start = performance.now();
+// iterative
+function iFib(n) {
+    const vals = [0, 1];
+    while (vals.length - 1 < n) {
+        let len = vals.length;
+        vals.push(vals[len - 1] + vals[len - 2]);
+    }
+    return vals[n];
+}
+console.log(iFib(20));
+console.log(`Iterative took ${performance.now() - start} milliseconds to run`);
+
--- 01-javascript/04-tiempo(opcional)/invertir_cadena.js
+++ 01-javascript/04-tiempo(opcional)/invertir_cadena.js
@@ -1,2 +1,22 @@
+// const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+// const reversed1 = story.split("").reverse().join("");
+
+
 const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
-const reversed1 = story.split("").reverse().join("");
+
+let start = performance.now();
+const reversed = story.split("").reverse().join("");
+
+console.log(reversed);
+console.log(`\nReversed took ${performance.now() - start} milliseconds to run \n`);
+
+//Invertir mas eficiente con iteración
+start = performance.now();
+let reversed2 = "";
+
+for (let i = story.length-1; i >= 0; i--) {
+    reversed2 += story[i];
+}
+
+console.log(reversed2);
+console.log(`\nReversed2 took ${performance.now() - start} milliseconds to run \n`);
